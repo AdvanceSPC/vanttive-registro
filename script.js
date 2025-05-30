6362793b0f16db5d5252e0b0d590bb6761d0cf6d
Layedra75 <layedra75@gmail.com>
2025-05-30 08:38:10 -0500

    Landing Page de Registro

---
+++ script.js
@@ -0,0 +1,15 @@
+
+// Generar particulas
+document.addEventListener("DOMContentLoaded", function () {
+  const particleContainer = document.querySelector(".particle-container");
+  const numParticles = 100;
+
+  for (let i = 0; i < numParticles; i++) {
+    const particle = document.createElement("div");
+    particle.classList.add("particle");
+    particle.style.left = `${Math.random() * 100}%`;
+    particle.style.top = `${Math.random() * 100}%`;
+    particle.style.animationDelay = `${Math.random() * 15}s`;
+    particleContainer.appendChild(particle);
+  }
+});
